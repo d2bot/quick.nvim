9ec91729ac1eee1d8219d0212ba87a522c1973b3
d2bot <d2botroot@gmail.com>
2022-05-04 21:43:29 +0800

    feature: kill buffer

--- lua/telescope-config.lua
+++ lua/telescope-config.lua
@@ -1,6 +1,13 @@
 local telescope = require("telescope")
 
 telescope.setup{
+  defaults = {
+    mappings = {
+      n = {
+    	  ['<C-D>'] = require('telescope.actions').delete_buffer
+      }
+    }
+  },
   pickers = {
     find_files = {
       theme = "ivy",
@@ -10,4 +17,4 @@ telescope.setup{
 
 vim.api.nvim_set_keymap('n', '<C-P>', "<cmd>lua require('telescope.builtin').find_files()<CR>", { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-F>', "<cmd>lua require('telescope.builtin').live_grep()<CR>", { noremap = true })
-vim.api.nvim_set_keymap('n', '<C-B>', "<cmd>lua require('telescope.builtin').buffers()<CR>", { noremap = true })
+vim.api.nvim_set_keymap('n', '<C-B>', "<cmd>lua require('telescope.builtin').buffers({ignore_current_buffer = true})<CR>", { noremap = true })
